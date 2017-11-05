82075e2a8eb68df455d2478ee84b733721c5435a
杨晓杰 <1127129489@qq.com>
2017-11-06 06:48:09 +0800

    debug

--- resources/views/layouts/common_home.blade.php
+++ resources/views/layouts/common_home.blade.php
@@ -889,13 +889,14 @@
             $('#galleryImg').attr('src',$(th).attr('src'));
             //计算galleryImg的宽高
             var w = $(window).width();
-
+            var hei = $(window).height();
             var img_w = $(th).width();//图片宽度
             var img_h = $(th).height();//图片高度
+            var top = (hei - img_h)/2;
             if (img_w > w) {//如果图片宽度超出指定最大宽度
                 var height = (w * img_h) / img_w; //高度等比缩放
                 $('#galleryImg').css( {
-                    "width" : w,"height" : height
+                    "width" : w,"height" : height,"top":top
                 });//设置缩放后的宽度和高度
             }
 
