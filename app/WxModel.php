4cdd352ad12b305b1b7795a7ca508e19414164b0
杨晓杰 <1127129489@qq.com>
2017-11-07 07:49:13 +0800

    debug

--- app/WxModel.php
+++ app/WxModel.php
@@ -147,11 +147,11 @@ class WxModel extends Model
         $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$token."&openid=".$openid."&lang=zh_CN";
         $userinfo = file_get_contents($url);
         $userinfo = json_decode($userinfo,true);
-        if(!$userinfo){
+        if(!$userinfo['headimgurl']){
             session([
                 'openid'=>null
             ]);
-            $this -> getUserInfo();
+            $this -> checkOpenid();
         }
         //dd($userinfo);
         return $userinfo;
