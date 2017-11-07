86c6d77eb4a74ef70e86473212202c8045ae4a11
杨晓杰 <1127129489@qq.com>
2017-11-07 20:47:58 +0800

    debug

--- resources/views/home/mall/buynow.blade.php
+++ resources/views/home/mall/buynow.blade.php
@@ -124,7 +124,13 @@
     wx.ready(function(){
 
 
-
+        wx.checkJsApi({
+            jsApiList: ['openAddress','editAddress'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+            success: function(res) {
+                // 以键值对的形式返回，可用的api值true，不可用为false
+                // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+            }
+        });
 
 
 
