9843eb3a66fb10adfb5d6c305cb974a54ffcd1da
杨晓杰 <1127129489@qq.com>
2017-11-07 23:11:52 +0800

    debug

--- config/wxsetting.php
+++ config/wxsetting.php
@@ -8,8 +8,8 @@ return [
     'url2' => 'http://m.tianluyangfa.com/laravel/public/home/mall', //商城跳转
     'url3' => 'http://m.tianluyangfa.com/laravel/public/home/market', //跳蚤市场
     'helpurl' => 'http://m.tianluyangfa.com/laravel/public/home/helpPage/', //帮助
-    'machid' => 1422146702,
-    'businesskey' => 'abcdefghigklmnopqrstuvwxyz123456',
+    'machid' => 1453827602,
+    'businesskey' => 'AAAAAAAAaaaaaaaa1111111100000000',
     'moban1' => '2MVJnykezlQT7wahfemeHg54KnYdyp36mmimn9TXTR8', //模版消息，后台审核
     'noticy_url' => 'http://m.tianluyangfa.com/laravel/public/home/mallNotice', //商城支付回调
 
