6ca20221fb46aacd36ae17ffb62b9f467d105e80
杨晓杰 <1127129489@qq.com>
2017-12-05 23:55:33 +0800

    update setting

--- config/wxsetting.php
+++ config/wxsetting.php
@@ -1,22 +1,22 @@
 <?php
 
 return [
-    'appid' => 'wx47ffe2d49c271963',
-    'secret' => '21e056fa1fc76ee67467247a8147d995',
-    'superurl' => 'http://m.tianluyangfa.com/laravel/public/homeJump', //授权跳转
-    'url1' => 'http://m.tianluyangfa.com/laravel/public/home', //首页跳转
-    'url2' => 'http://m.tianluyangfa.com/laravel/public/home/mall', //商城跳转
-    'url3' => 'http://m.tianluyangfa.com/laravel/public/home/market', //跳蚤市场
-    'url4' => 'http://m.tianluyangfa.com/laravel/public/home/myorder', //商城支付完毕发送模版消息进入订单
-    'url5' => 'http://m.tianluyangfa.com/laravel/public/home/myorder', //发货
-    'helpurl' => 'http://m.tianluyangfa.com/laravel/public/home/helpPage/', //帮助
+    'appid' => 'wx7ea0b386bde93f94',
+    'secret' => '83d1e9ab61a9c30fa9d08f160e5c6f78',
+    'superurl' => 'http://www.szyeweihui.com/public/homeJump', //授权跳转
+    'url1' => 'http://www.szyeweihui.com/public/home', //首页跳转
+    'url2' => 'http://www.szyeweihui.com/public/home/mall', //商城跳转
+    'url3' => 'http://www.szyeweihui.com/public/home/market', //跳蚤市场
+    'url4' => 'http://www.szyeweihui.com/public/home/myorder', //商城支付完毕发送模版消息进入订单
+    'url5' => 'http://www.szyeweihui.com/public/home/myorder', //发货
+    'helpurl' => 'http://www.szyeweihui.com/public/home/helpPage/', //帮助
     'machid' => 1453827602,
     'businesskey' => 'AAAAAAAAaaaaaaaa1111111100000000',
     'moban1' => '2MVJnykezlQT7wahfemeHg54KnYdyp36mmimn9TXTR8', //模版消息，后台审核
-    'noticy_url' => 'http://m.tianluyangfa.com/laravel/public/payNotify', //商城支付回调
-    'noticy_url_service' => 'http://m.tianluyangfa.com/laravel/public/payServiceNotify', //服务支付回调
-    'youlin_url' => 'http://m.tianluyangfa.com/laravel/public/home/pinlun/',//友邻互助详情
-    'myorder_url' => 'http://m.tianluyangfa.com/laravel/public/home/myorder',//我的订单
+    'noticy_url' => 'http://www.szyeweihui.com/public/payNotify', //商城支付回调
+    'noticy_url_service' => 'http://www.szyeweihui.com/public/payServiceNotify', //服务支付回调
+    'youlin_url' => 'http://www.szyeweihui.com/public/home/pinlun/',//友邻互助详情
+    'myorder_url' => 'http://www.szyeweihui.com/public/home/myorder',//我的订单
 
 ];
 
