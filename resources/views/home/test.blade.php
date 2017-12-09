906dd826ced5945e3bd0a878a37c4bc3bdd7e4a0
杨晓杰 <1127129489@qq.com>
2017-12-09 09:30:32 +0800

    debug

--- resources/views/home/test.blade.php
+++ resources/views/home/test.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh-CN">
 <head>
     @include('layouts.common')
-    <title>系统维护中</title>
+    <title>系统升级中</title>
 
 </head>
 <body>
@@ -13,7 +13,7 @@
     <img src="{{asset('images/logo.png')}}" />
 </header>
 <article class="search-page" id="search-box" style="top:43px;height:100%;line-height:100%;text-align:center;">
-    <p style="margin-top:50%;text-align: center">系统维护中</p>
+    <p style="margin-top:50%;text-align: center">系统升级中</p>
 </article>
 
 </body>
