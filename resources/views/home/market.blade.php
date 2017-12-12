0d1db6801e752a6ade68959825c0ba6f44753d5c
杨晓杰 <1127129489@qq.com>
2017-12-12 23:15:03 +0800

    de

--- resources/views/home/market.blade.php
+++ resources/views/home/market.blade.php
@@ -56,7 +56,7 @@
                 @foreach($vo['img'] as $vol)
 
                     <div>
-                        <img style="width: auto;  height: auto;display:inline-block;  " class="lazy" src="{{ asset('images/lazyload.png') }}" data-original="{{asset('images').'/'.$vol}}" onclick="showimg(this)" />
+                        <img style="width:100%;  height: 100%;display:inline-block;  " class="lazy" src="{{ asset('images/lazyload.png') }}" data-original="{{asset('images').'/'.$vol}}" onclick="showimg(this)" />
                     </div>
                 @endforeach
             </div>
