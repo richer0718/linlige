367c3ed07da10ea922a23709c4f05377d0905dbb
杨晓杰 <1127129489@qq.com>
2018-01-10 22:03:38 +0800

    debug

--- resources/views/home/malldetail.blade.php
+++ resources/views/home/malldetail.blade.php
@@ -7,13 +7,17 @@
         wx.config(<?php echo $js->config(array('onMenuShareAppMessage', 'onMenuShareWeibo'), true) ?>);
     </script>
     <script>
-        wx.checkJsApi({
-            jsApiList: ['onMenuShareAppMessage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-            success: function(res) {
-                // 以键值对的形式返回，可用的api值true，不可用为false
-                // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-            }
+        wx.ready(function(){
+            // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+            wx.checkJsApi({
+                jsApiList: ['onMenuShareAppMessage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+                success: function(res) {
+                    // 以键值对的形式返回，可用的api值true，不可用为false
+                    // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+                }
+            });
         });
+
 
         wx.onMenuShareAppMessage({
             title: '分享标题', // 分享标题
